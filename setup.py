bbd56a37cf1f5bc792329918f4d27fb0ee63f4d6
AlexWoIf <alexey.barannikov@gmail.com>
2024-11-21 18:23:25 +0300

    fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='src',
     version='0.1.0',
-    description='DrWeb task for position Puthon developer',
+    description='DrWeb task for position Python developer',
 
     author='Alexey Barannikov',
     author_email='alexwolf@inbox.ru',
